ffb101f23523f1b67dd4517160d19bf1b9d87f02
Lauro Weingartner Neto <lauro.neto@zpesystems.com>
2026-02-19 08:46:54 -0300

    Removing changes on the overlay

--- src/api/event/errors.ts
+++ src/api/event/errors.ts
@@ -50,3 +50,24 @@ export function getEventsErrorMessage(error: AppError, t: TFunction): string {
       return t("api.errors.unknownError")
   }
 }
+
+export function getUserEventsErrorMessage(error: AppError, t: TFunction): string {
+  const code = error.code
+
+  switch (code) {
+    case "INVALID_REQUEST":
+      return t("errors.event.getUserEvents.invalidRequest")
+    case "INVALID_PAGINATION":
+      return t("errors.event.getUserEvents.invalidPagination")
+    case "INVALID_ACTIVE_FILTER":
+      return t("errors.event.getUserEvents.invalidActiveFilter")
+    case "UNAUTHORIZED":
+      return t("errors.event.unauthorized")
+    case "INTERNAL_SERVER_ERROR":
+      return t("errors.event.internalError")
+    case "NETWORK_ERROR":
+      return t("api.errors.networkError")
+    default:
+      return t("api.errors.unknownError")
+  }
+}
